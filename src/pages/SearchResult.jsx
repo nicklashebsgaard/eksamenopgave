6940dc379f1e8739a26d22503f0a880cc9d1304d
nicklas hebsgaard <nicklashebsgaard@gmail.com>
2022-11-12 11:22:27 +0100

    søge med lidt styles

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -2,61 +2,60 @@
 import "./searchresult.scss";
 
 // REACT useState, useEffect
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 // LINK
 import { Link } from "react-router-dom";
 
-//COMPONENTS
-import ErrorMessage from '../components/errormessage/ErrorMessage';
-import Loader from '../components/loader/Loader';
+// PARSER
+import Parser from "html-react-parser";
 
-// API 
-import { searchQ } from '../helpers/apikald';
+//COMPONENTS
+import ErrorMessage from "../components/errormessage/ErrorMessage";
+import Loader from "../components/loader/Loader";
+
+// API
+import { searchQ } from "../helpers/apikald";
 
 // useParams for søge på input
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 const SearchResult = () => {
+  // tjek i App hvad param hedder - fx search/:q
+  let { q } = useParams();
 
-    // tjek i App hvad param hedder - fx search/:q
-    let {q} = useParams()
+  // STATE
+  const [soeg, setSoeg] = useState();
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    // STATE
-    const [soeg, setSoeg] = useState()
-    const [error, setError] = useState(false)
-    const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true);
 
-    useEffect(() => {
-      
-        setLoading(true)
-
-        searchQ(q)
-        .then((responsedata) => {
-            /* concat tager begge arrays og sætter dem til et arry */
-            let result = responsedata.data.searchresult.news.concat(responsedata.data.searchresult.services)
-            setSoeg(result)
-            setError(false)
-        })
-        .catch(() => {
-            setError(true)
-            setSoeg()
-        })
-        .finally(() =>{
-            setLoading(false)
-        })
-
-    }, [q])
-    
+    searchQ(q)
+      .then((responsedata) => {
+        /* concat tager begge arrays og sætter dem til et arry */
+        let result = responsedata.data.searchresult.news.concat(
+          responsedata.data.searchresult.services
+        );
+        setSoeg(result);
+        setError(false);
+      })
+      .catch(() => {
+        setError(true);
+        setSoeg();
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [q]);
 
   return (
-
-    <section className='SearchResult'>
+    <section className="SearchResult">
       {error && <ErrorMessage />}
       {loading && <Loader />}
-      
-      <div className="flexContainer">
 
+      <div className="flexContainer">
         <div className="titleText">
           <h1>Dit søgeresultat</h1>
         </div>
@@ -74,20 +73,39 @@ const SearchResult = () => {
             </Link>
           </ul>
         </div>
-
       </div>
 
-        {
-            soeg && soeg.map(s => 
-                <div key={s._id}>
-                <h2 >{s.title}</h2>
+      <div className="wrapper">
+        {soeg &&
+          soeg.map((s, i) => (
 
-                <div>{s.teaser}</div>
 
+              <div className="cardContainer" key={"soeg" + i}>
+                <div className="newsCard" >
+
+                  <div className="imageContainer">
+                    <img
+                      src={"http://localhost:5333/images/news/" + s.image}
+                      alt="News"
+                      loading="lazy"
+                    />
+                  </div>
+
+                  <div className="textContainer">
+                    <h3>{s.title}</h3>
+                    <p>{s.content.slice(0, 100)}...</p>
+                  </div>
                 </div>
-            )
-        }
+              </div>
 
+
+
+              
+
+          ))}
+
+      </div>
+      
     </section>
 
   );
